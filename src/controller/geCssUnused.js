c3307372e09e6ea5a3345ac5ae2fec2c2555dcd3
Joseg <jgracia9988@gmail.com>
2019-11-28 11:41:02 +0100

    reorganized the main code

--- src/controller/geCssUnused.js
+++ src/controller/geCssUnused.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 export async function getUnusedCss(cssFiles, idsFoundHTML, classFoundHTML) {
     let idsInCss = [];
     let classInCss = [];
+    const unusedArray = [];
+    let classNotUsed = [];
+    let idsNotUsed = [];
 
 
     // Find all css and classes created (we do not know if used yet)
@@ -12,22 +15,22 @@ export async function getUnusedCss(cssFiles, idsFoundHTML, classFoundHTML) {
         classInCss = classInCss.concat(texto.match(/\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*(?=[^}]*\{)/g));
     }
 
-    // check if the ids are used
-    let idsNotUsed = [];
+    // check if the ids are used if not add the selector to 'idsNotUsed'
     for (let i = 0; i < idsInCss.length; i++) {
-        if (idsFoundHTML.indexOf(idsInCss[i].substr(1)) === -1) { // if it doesn't found an used id...
+        if (idsFoundHTML.indexOf(idsInCss[i].substr(1)) === -1) { // if it hasn't been used
             idsNotUsed = idsNotUsed.concat(idsInCss[i]); // adds it to the array of unused
         }
     }
 
-    let classNotUsed = [];
+    // check if the classes are used if not add the selector to 'classNotUsed'
     for (let i = 0; i < classInCss.length; i++) {
-        if (classFoundHTML.indexOf(classInCss[i].substr(1)) === -1) { // if it doesn't found an used class...
+        if (classFoundHTML.indexOf(classInCss[i].substr(1)) === -1) { // if it hasn't been used
             classNotUsed = classNotUsed.concat(classInCss[i]); // adds it to the array of unused
         }
     }
-    console.log(idsNotUsed);
-    console.log(classNotUsed);
+
+    unusedArray.push(idsNotUsed, classNotUsed);
+    return unusedArray;
 }
 
 export default getUnusedCss;
