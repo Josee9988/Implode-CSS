95190edafb3aa5a87f2bf882a9a291ba3eaca3d0
Joseg <jgracia9988@gmail.com>
2019-11-30 14:45:50 +0100

    showing unused css in the table works

--- src/server/public/assets/js/dataController.js
+++ src/server/public/assets/js/dataController.js
@@ -1,2 +1,26 @@
-const unusedCss = (contents);
-console.log(unusedCss);
+/* eslint-disable prefer-spread */
+/* eslint-disable no-undef */
+
+function createUnusedTemplate(element) {
+    let type = '';
+    if (element.substr(0, 1) === '#') { // if ii is an id:
+        type = 'Id';
+    } else {
+        type = 'Class';
+    }
+    return `<td>${type}</td>
+            <td>${element.substr(1)}</td>`;
+}
+
+
+window.addEventListener('load', () => {
+    const dataTable = document.getElementById('dataTable');
+    unusedCss = [].concat.apply([], contents); // FROM 2D array to 1D
+
+    unusedCss.forEach((content) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = createUnusedTemplate(content);
+        dataTable.appendChild(tr);
+    });
+
+});
