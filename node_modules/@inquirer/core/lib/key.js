a09e4c44335fa179e7f9a045c5151d6797947ff9
Joseg <jgracia9988@gmail.com>
2019-12-07 16:58:30 +0100

    removed node_modules

--- node_modules/@inquirer/core/lib/key.js
+++
@@ -1,9 +0,0 @@
-exports.isUpKey = key =>
-  key.name === 'up' || key.name === 'k' || (key.name === 'p' && key.ctrl);
-
-exports.isDownKey = key =>
-  key.name === 'down' || key.name === 'j' || (key.name === 'n' && key.ctrl);
-
-exports.isSpaceKey = key => key.name === 'space';
-
-exports.isNumberKey = key => '123456789'.includes(key.name);
